e143cbb1535277abb2113d619fdc88f07268115d
Your Name <you@example.com>
2018-04-26 11:31:08 -0700

    Use puppeteer

--- test/google.test.js
+++ test/google.test.js
@@ -1,7 +1,13 @@
 import { expect } from 'chai';
+import puppeteer from 'puppeteer';
 
 describe('Google', () => {
-  it('searches for stuff', () => {
-    expect(true).to.eq(true);
+  it('searches for stuff', async () => {
+    const browser = await puppeteer.launch({ headless: false });
+
+    const page = await browser.newPage();
+    await page.goto('https://www.google.com/ncr');
+
+     await browser.close();
   });
 });
